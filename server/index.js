fdc9392a53a24c8dc0e8f09c045cdedef987a5b2
Keith Mair <mairck@gmail.com>
2022-02-08 15:31:25 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+const cors = require(`cors`);
+const ctrl = require(`./controller`)
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get(`/api/houses`, ctrl.getHouse);
+app.delete(`/api/houses/:id`, ctrl.deleteHouse)
+app.post(`/api/houses/`, ctrl.createHouse);
+app.put(`/api/houses/:id`, ctrl.updateHouse);
+
+
+const port = 4004;
+app.listen (port, () => console.log (`Listening on port ${port}`));
